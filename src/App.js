42e129467f7eb2dea426431fd28a0b1336da95b9
배성진 <baeseongjin0311@gmail.com>
2023-06-15 00:16:36 +0900

    new

--- src/App.js
+++ src/App.js
@@ -1,19 +1,11 @@
 import "./App.css";
 import "./GNH/header";
 import HeaderMenu from "./GNH/header";
-import Navbar from "./GNN/nav";
 
 function App() {
   return (
     <div>
       <HeaderMenu></HeaderMenu>
-      {/* <nav className="navbar">
-        <button
-          className="toggle"
-          onClick={() => setNavbarOpen((prev) => !prev)}
-        ></button>
-        <ul></ul>
-      </nav> */}
     </div>
   );
 }
